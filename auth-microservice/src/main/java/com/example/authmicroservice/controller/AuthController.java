e908e1fa1d25675bc4bf842531a235239caddaf9
Ahmet Sami Yeniçeri <ahmet.sami.yeniceri@ericsson.com>
2024-09-02 15:49:57 +0300

    swagger added & dependency update

--- auth-microservice/src/main/java/com/example/authmicroservice/controller/AuthController.java
+++ auth-microservice/src/main/java/com/example/authmicroservice/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.example.authmicroservice.dto.request.RegisterRequestDto;
 import com.example.authmicroservice.dto.request.LoginRequestDto;
 import com.example.authmicroservice.model.Auth;
 import com.example.authmicroservice.service.AuthService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "AuthController", description = "AuthController Operations")
 @RestController
 @RequestMapping(RestApis.AUTHSERVICE)
 public class AuthController {
@@ -21,6 +23,7 @@ public class AuthController {
         this.service = service;
     }
 
+    @Tag(name = "Register")
     @PostMapping(RestApis.REGISTER)
     public ResponseEntity<Auth> register(@RequestBody RegisterRequestDto registerDto) {
         if (!registerDto.getUserName().equals(registerDto.getUserName()) || !registerDto.getPassword().equals(registerDto.getPassword())) {
@@ -29,6 +32,7 @@ public class AuthController {
         return new ResponseEntity<>(service.register(registerDto), HttpStatus.OK);
     }
 
+    @Tag(name = "Login")
     @PostMapping(RestApis.LOGIN)
     public ResponseEntity<Boolean> login(@RequestBody LoginRequestDto loginDto) {
         return new ResponseEntity<>(service.login(loginDto), HttpStatus.OK);
