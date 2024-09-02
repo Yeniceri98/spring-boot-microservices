0925885d938d9c6a7b91259284fde3b0272b9e39
Ahmet Sami Yeniçeri <ahmet.sami.yeniceri@ericsson.com>
2024-09-02 16:55:47 +0300

    swagger added

--- user-microservice/src/main/java/com/example/usermicroservice/conroller/UserProfileController.java
+++ user-microservice/src/main/java/com/example/usermicroservice/conroller/UserProfileController.java
@@ -4,12 +4,14 @@ import com.example.usermicroservice.config.RestApis;
 import com.example.usermicroservice.document.UserProfile;
 import com.example.usermicroservice.dto.CreateUserRequestDto;
 import com.example.usermicroservice.service.UserProfileService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "UserController", description = "UserController Operations")
 @RestController
 @RequestMapping(RestApis.USERPROFILE)
 public class UserProfileController {
@@ -19,12 +21,14 @@ public class UserProfileController {
         this.service = service;
     }
 
+    @Tag(name = "Create User")
     @PostMapping(RestApis.CREATE_USER)
     public ResponseEntity<Boolean> createUser(@RequestBody CreateUserRequestDto createUserDto) {
         service.createUser(createUserDto);
         return new ResponseEntity<>(true, HttpStatus.CREATED);
     }
 
+    @Tag(name = "Get All Users")
     @GetMapping(RestApis.GET_ALL)
     public ResponseEntity<List<UserProfile>> getAllUsers() {
         return new ResponseEntity<>(service.getAllUsers(), HttpStatus.OK);
