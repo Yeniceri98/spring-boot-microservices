8dcc859adea047601d70e5b14523c33f899140b3
Ahmet Sami Yeniçeri <ahmet.sami.yeniceri@ericsson.com>
2024-09-04 11:13:00 +0300

    initial commit for git config server

---
+++ git-config-server/src/main/java/com/example/gitconfigserver/GitConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitConfigServerApplication.class, args);
+	}
+
+}
